4b251774b761364adeb6f2ea8a350d243e13474c
Isaque-Morais <isaquemorais5@gmail.com>
2022-12-26 23:41:40 -0300

    Adicionando e removendo arrays

--- Matriculas.php
+++ Matriculas.php
@@ -1,10 +1,31 @@
 <?php
 
-$notasBrimestre1 = [
-    'Isaque'    => 10, 
-    'Lucas'     => 9,
-    'Ricardo'   => 8,
-    'Maria'     => 7,
-    'Nicole'    => 6,
+$alunos2021 = [
+    'Isaque',     
+    'Lucas',    
+    'Ricardo',   
+    'Maria',     
+    'Nicole',    
 ];
 
+$novosAlunos = [
+    'Patricia',
+    'Nico',
+    'Kilderson',
+    'Daiane',
+];
+
+// Adicionando itens entre dois arrays, ou seja entre dois arrays
+$alunos2022 = [...$alunos2021, 'Isaque Morais', ...$novosAlunos];
+
+// Adiciona itens no final de um array
+array_push($alunos2022, 'Alice', 'Bob', 'Charlie');
+$alunos2022[] = 'Luiza';
+
+array_unshift($alunos2022, 'Rafaela', 'Bruna');
+
+echo array_shift($alunos2022) . PHP_EOL;
+
+array_pop($alunos2022);
+
+var_dump($alunos2022);
